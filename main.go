628ea1d032d92b335cf163852e6ef88935dd6f78
user <user@local.domain>
2023-04-08 01:14:42 -0400

    update

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	res, err := glua.NewAction().WithScript(`
 		local iter = require 'iter'
 		function sum(n)
-		return iter.range(1000000):drop(100000):take(100000):reduce(iter.op.add, 0)
+			return iter.range(n):drop(n/10):take(n/10):reduce(iter.op.add, 0)
 		end
 	`).WithEntrypoint("sum").AddParam(10000000).Execute(context.Background())
 
