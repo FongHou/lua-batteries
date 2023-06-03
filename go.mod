c99e2e1907f7ebad09d4f29c96774bd93cc62a8a
user <user@local.domain>
2023-06-03 19:29:53 -0400

    update

--- go.mod
+++ go.mod
@@ -2,12 +2,4 @@ module example.com/main
 
 go 1.20
 
-require (
-	github.com/RyouZhang/go-lua v0.4.6
-	github.com/fffonion/ljpack v0.0.0-20220616082635-5021c84f081e
-)
-
-require (
-	github.com/mattn/go-sqlite3 v1.14.16 // indirect
-	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-)
+require github.com/RyouZhang/go-lua v0.4.7
--- main.go
+++ main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"time"
-
 	glua "github.com/RyouZhang/go-lua"
 )
 
-// #cgo LDFLAGS: -static-libgcc -L/usr/local/lib -lluajit-5.1 -L${SRCDIR} -Wl,--whole-archive -lluabatteries -Wl,--no-whole-archive
+// #cgo LDFLAGS: -static-libgcc -L/usr/local/lib -lluajit-5.1 -L${SRCDIR} -Wl,--whole-archive -lluabatteries -Wl,--no-whole-archive -Wl,-E
 import "C"
 
 func main() {
-	ts := time.Now()
-	res, err := glua.NewAction().WithScript(`
-		local iter = require 'iter'
-		function sum(n)
-			return iter.range(n):drop(n/10):take(n/10):reduce(iter.op.add, 0)
+	opts := glua.NewOptions().SetPreloadScripeMethod(func() string {
+		return `
+		local ok, fennel = pcall(require, 'fennel')
+		if ok then
+			print(fennel.runtimeVersion())
+			fennel.install()
+			debug.traceback = fennel.traceback
 		end
-	`).WithEntrypoint("sum").AddParam(10000000).Execute(context.Background())
 
-	fmt.Println("cost:", time.Since(ts))
-	fmt.Println(res, err)
+		ok, JSON = pcall(require, 'rapidjson')
+		if not ok then
+			JSON = require('json')
+			JSON.null = nil
+		end
+
+		require('batteries')()
+		`
+	})
+	glua.GlobalOptions(opts)
 }
